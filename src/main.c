780725a616f81a9aa7d9bffeecdf2e8cac48d3e4
Francisco Soares <francisco.ssn94@gmail.com>
2026-08-12 20:28:04 -0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -87,8 +87,9 @@ int main() {
                                       response_flags, 1, 1, 0, 0);
     DNSQuestion question = dns_question_new(1, 1);
     DNSAnswer answer = dns_answer_new(1, 1, 60, 4);
-    DNSMessage message = dns_message_new(header, "codecrafters.io", question,
-                                         "codecrafters.io", answer, "8.8.8.8");
+    DNSMessage message =
+        dns_message_new(header, received_message.label, question,
+                        received_message.label, answer, "8.8.8.8");
 
     size_t message_length = 0;
     uint8_t *msg = dns_message_to_buffer(message, &message_length);
